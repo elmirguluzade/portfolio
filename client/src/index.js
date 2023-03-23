8284b7a522aa0f5f1110c07e841baf0bd1f10ccd
Elmir <elmir.quluzade627@gmail.com>
2023-03-23 23:55:21 +0400

    vercel analysis

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<><App /></>);
